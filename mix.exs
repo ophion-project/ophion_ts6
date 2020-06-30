d62b5b2e7ae5cd6ed63440e152e886d2ca36c550
Ariadne Conill <ariadne@dereferenced.org>
2020-06-30 11:43:16 -0600

    mix: add ophion_ircv3 dep

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule OphionTs6.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      { :custom_base, "~> 0.2" }
+      { :custom_base, "~> 0.2" },
+      { :ophion_ircv3, "~> 0.1" }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
