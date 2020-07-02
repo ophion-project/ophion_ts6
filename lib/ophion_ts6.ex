b626525e5be61e535b95a73f21cd5d84a1948bcd
Ariadne Conill <ariadne@dereferenced.org>
2020-07-01 20:43:00 -0600

    remove useless example tests

--- lib/ophion_ts6.ex
+++ lib/ophion_ts6.ex
@@ -1,18 +1,2 @@
 defmodule Ophion.TS6 do
-  @moduledoc """
-  Documentation for `Ophion.TS6`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ophion.TS6.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ophion_ts6_test.exs
+++ test/ophion_ts6_test.exs
@@ -1,8 +1,4 @@
 defmodule Ophion.TS6.Test do
   use ExUnit.Case
   doctest Ophion.TS6
-
-  test "greets the world" do
-    assert Ophion.TS6.hello() == :world
-  end
 end
