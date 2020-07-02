96b405c3fea34e9c9133aeec7994872c7c740565
Ariadne Conill <ariadne@dereferenced.org>
2020-07-01 21:31:10 -0600

    state machine: add enough to make the state machine boot and process messages

--- lib/ophion_ts6/state_machine.ex
+++ lib/ophion_ts6/state_machine.ex
@@ -3,9 +3,13 @@ defmodule Ophion.TS6.State do
     :name,
     :sid,
     :uid_generator,
+    :connecting_server,
+    capabs: ["IE", "EX", "QS", "IRCX", "EUID"],
+    required_capabs: ["IE", "EX", "QS", "EUID"],
     clients: %{},
     channels: %{},
-    servers: %{}
+    servers: %{},
+    commands: %{}
   ]
 
   def validate(%__MODULE__{name: nil}), do: {:error, {:invalid_state, :invalid_name}}
@@ -14,6 +18,8 @@ defmodule Ophion.TS6.State do
 end
 
 defmodule Ophion.TS6.StateMachine do
+  require Logger
+
   use GenServer
 
   alias Ophion.IRCv3.Message
@@ -39,6 +45,26 @@ defmodule Ophion.TS6.StateMachine do
     GenServer.cast(pid, {:process, message})
   end
 
+  def process(pid, messages) when is_binary(messages) do
+    messages
+    |> String.split("\n")
+    |> Enum.each(fn msg ->
+      {:ok, parsed} =
+        msg
+        |> String.trim()
+        |> Ophion.IRCv3.parse()
+
+      process(pid, parsed)
+    end)
+  end
+
+  @doc """
+  Attaches a command receiver callback at runtime.
+  """
+  def attach_command(pid, command, receiver) do
+    GenServer.cast(pid, {:attach, command, receiver})
+  end
+
   @doc """
   Generate a burst transaction which describes the current state machine.
 
@@ -49,6 +75,13 @@ defmodule Ophion.TS6.StateMachine do
     GenServer.call(pid, {:burst, excluding})
   end
   def burst(pid), do: burst(pid, nil)
+
+  @doc """
+  Fetch the current state from the state machine.
+  """
+  def fetch(pid) do
+    GenServer.call(pid, {:fetch})
+  end
 
   # callbacks
   def init(%State{} = state) do
@@ -68,8 +101,10 @@ defmodule Ophion.TS6.StateMachine do
   end
 
   defp handle_message(%State{} = state, %Message{} = message) do
+    Logger.debug("#{inspect(__MODULE__)}: processing #{inspect(message)} @#{inspect(self())}")
+
     state =
-      (state.listeners[message.verb] || [])
+      (state.commands[message.verb] || [])
       |> Enum.reduce_while(state, fn callee, state ->
         callee.(state, message)
       end)
@@ -77,8 +112,12 @@ defmodule Ophion.TS6.StateMachine do
     {:ok, state}
   end
 
+  def handle_call({:fetch}, _from, %State{} = state) do
+    {:reply, state, state}
+  end
+
   # XXX: implement these
-  def handle_call({:burst, _excluding}, %State{} = _state) do
+  def handle_call({:burst, _excluding}, _from, %State{} = _state) do
     {:reply, "\r\n"}
   end
 end
