0cd831ed6b09463f32c6a93f97b790ed716b77fb
Ariadne Conill <ariadne@dereferenced.org>
2020-07-02 19:51:14 -0600

    state: add State.link_server/2 convenience function, shorten name aliases to SID pointers

--- lib/ophion_ts6/state.ex
+++ lib/ophion_ts6/state.ex
@@ -38,11 +38,22 @@ defmodule Ophion.TS6.State do
   end
 
   @doc "Retrieves a server instance from the state by SID or name."
-  def get_server(%__MODULE__{} = state, sid_or_name), do: state.global_servers[sid_or_name]
+  def get_server(%__MODULE__{} = state, sid_or_name) do
+    case state.global_servers[sid_or_name] do
+      %Server{} = server ->
+        server
+
+      sid when is_binary(sid) ->
+        get_server(state, sid)
+
+      _ ->
+        nil
+    end
+  end
 
   @doc "Orphan a server from the global server list as well as its parent."
   def delete_server(%__MODULE__{} = state, %Server{} = server) do
-    with {:parent, %Server{} = parent} <- get_server(state, server.parent_sid) do
+    with {:parent, %Server{} = parent} <- {:parent, get_server(state, server.parent_sid)} do
       # delete descendents
       state =
         Enum.reduce(server.servers, state, fn child_sid, state ->
@@ -75,11 +86,18 @@ defmodule Ophion.TS6.State do
   end
 
   def put_server(%__MODULE__{} = state, %Server{parent_sid: nil} = root) do
-    Map.put(state, :root, root)
+    global_servers =
+      state.global_servers
+      |> Map.put(root.sid, root)
+      |> Map.put(root.name, root.sid)
+
+    state
+    |> Map.put(:root, root)
+    |> Map.put(:global_servers, global_servers)
   end
 
   def put_server(%__MODULE__{} = state, %Server{} = server) do
-    with {:parent, %Server{} = parent} <- get_server(state, server.parent_sid) do
+    with {:parent, %Server{} = parent} <- {:parent, get_server(state, server.parent_sid)} do
       parent =
         parent
         |> Server.add_child(server)
@@ -91,13 +109,29 @@ defmodule Ophion.TS6.State do
       # update the state with the new global_servers table
       global_servers =
         state.global_servers
-        |> Map.delete(server.sid)
-        |> Map.delete(server.name)
+        |> Map.put(server.sid, server)
+        |> Map.put(server.name, server.sid)
 
       Map.put(state, :global_servers, global_servers)
     else
       {:parent, nil} ->
         Logger.warn("#{inspect(__MODULE__)}: could not find parent SID #{server.parent_sid} of server #{server.sid}/#{server.name}!!!")
     end
+  end
+
+  @doc "A convenience function which updates a server's parent and links it into the graph."
+  def link_server(%__MODULE__{} = state, %Server{} = parent, %Server{} = child) do
+    child =
+      child
+      |> Map.put(:parent_sid, parent.sid)
+
+    state =
+      state
+      |> put_server(child)
+
+    parent = get_server(state, parent.sid)
+    child = get_server(state, child.sid)
+
+    {:ok, state, parent, child}
   end
 end
